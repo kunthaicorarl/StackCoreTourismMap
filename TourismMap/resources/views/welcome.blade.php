15839d3d63a1c80f3c666e43826cd2108398cdb2
Phon Kunthai <kunthai.corarl@gmail.com>
2017-09-10 13:02:47 +0700

    add tourism

--- TourismMap/resources/views/welcome.blade.php
+++ TourismMap/resources/views/welcome.blade.php
@@ -15,6 +15,7 @@
   <link href="{{ asset('css/home-page-common.css') }}" rel="stylesheet"/> 
     <link href="{{ asset('css/slider.css') }}" rel="stylesheet"/>  
         <link href="{{ asset('js/slider.js') }}" rel="stylesheet"/>  
+          <link href="{{ asset('common.css') }}" rel="stylesheet"/>  
              <link href="{{ asset('css/footer-link-style.css') }}" rel="stylesheet"/>  
         <!-- Styles -->
         <style>
@@ -145,7 +146,7 @@ src: url('../fonts/radikal_font_family/OPENSANS-SEMIBOLD.TTF');
 .form-horizontal_x .col-sm-2 {    padding: 3px;}
 .jain_container .navbar-default{position: absolute;    top:0px;     background-color: rgba(255, 255, 255, 0.72);}
 
-.jain_container .nav>li>a{padding: 15px 10px;color:#4c1f1e;font-family: RadikalW01_Black_bold;}
+.jain_container .nav>li>a{padding: 15px 10px;color:white;font-family: RadikalW01_Black_bold;}
 
 .jain_container .btn-default {
     color: #fff;
@@ -158,7 +159,7 @@ src: url('../fonts/radikal_font_family/OPENSANS-SEMIBOLD.TTF');
     padding: 3px;
 
 }
-.jain_container .navbar-nav{    margin-top: 12px;}
+/*.jain_container .navbar-nav{    margin-top: 12px;} */
 .jain_container .btn-default:hover{ background:#000; border-color:#000;}
 .medias ul li a {
     height: 30px;
@@ -496,23 +497,12 @@ body {
     </head>
     <body>
         <div class="flex-center position-ref">
-            {{--  @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif  --}}
-<!---->
-
+        
 <link rel="stylesheet" href="http://mogulsdemo.com/html/pride-home/css/font-awesome.css">
 
 <div class="jain_container">
 
-<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-default navbar-default-header navbar-fixed-top" role="navigation">
 <div class="container1 container-fluid"> 
 <div class="navbar-header">
 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -525,11 +515,9 @@ body {
 
  <div class="col-md-8">
 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
 <div class="col-md-12">
 <ul class="nav navbar-nav">
-
+<li><a href="#">TourismMap.com</a></li>
 <li class="dropdown mega-dropdown">
     			<a href="#" class="dropdown-toggle" data-toggle="dropdown">FIND YORU HOTEL <span class="caret"></span></a>				
 				<ul class="dropdown-menu mega-dropdown-menu">
@@ -570,93 +558,17 @@ body {
 
 
 <li><a href="#">SPECIAL OFFERS</a></li><li>
-<a href="#">PLAN YOUR EVENTS</a></li><li>
-<a href="#">OUR BRANDS</a></li>
-
-<li><a href="#">LOYALTY</a></li>
+<a href="#">PLAN YOUR EVENTS</a></li>
 <li>
 <a href="#">CONTACT US</a>
 </li>
 
 </ul>
 </div>
-<div class="col-md-12">
-<div class="form-horizontal form-horizontal_x">
-<div class="col-md-12">
-    <div class="form-group">
-     
-      <div class="col-sm-4">
-	 <select  name="tourism" id="tourism"  class="form-control" onchange="changeFormaction2(this.value);">
-                        <option value="" selected="">----Select Province---</option>
-                                                <option value="67961">Pride Plaza Hotel Ahmedabad</option>
-                                                <option value="67964">The Pride Hotel Bengaluru</option>
-                                                <option value="29191">The Pride Hotel Nagpur</option>
-                                                <option value="67963">The Pride Hotel Chennai</option>
-                                                <option value="29190">The Pride Hotel, Pune</option>
-                                                <option value="67962">Pride Plaza Hotel Kolkata</option>
-                                                <option value="67580">Pride Plaza Hotel Delhi</option>
-                                                <option value="8">Pride Amber Vilas Jaipur</option>
-                                                <option value="65728">Pride Surya Mountain Resort Mcleodganj</option>
-                                                <option value="60533">Pride Sun Village Resort SPA,Goa</option>
-                                                <option value="138227">The Pride Biznotel,Vadodara</option>
-                                        </select>
-	  
-        <!-- <input type="text" class="form-control" id="email" placeholder="Enter email"> -->
-      </div>
-	  
-	  <div class="col-sm-3">          
-        <input type="text" class="form-control" id="pwd" placeholder="Place Pointer">
-      </div>
-	  
-	  {{--  <div class="col-sm-3">          
-<div class="content">
-<input type="text" class="search" id="searchid" placeholder="Search for people" />&nbsp; &nbsp; Ex:arunkumar, shanmu, vicky<br /> 
-
-</div>
-      </div>  --}}
-	  
-	  <div class="col-sm-2">
-      {{--  <button type="submit" class="btn btn-default">Visit Now</button>  --}}
-          <button id="visitedNow" class="btn btn-default">Visit Now</button>
-      </div>
-    </div>
-	</div>
-	
-  </div>
-  </div>
-  
 </div>
 </div>
-
-<div class="col-md-2 medias">
-
-<ul> 
-<li> <a href=""> <i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-<li> <a href=""><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-<li> <a href=""> <i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-<li> <a href=""> <i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-
-</ul>
-
-<div class="classus">
-
-<p> Call us +91 22 39178077</p>
-
 </div>
-
-<div class="why_bookonline">
-<p> Why Bookonline?</p>
-
-</div>
-
-</div>
-
- 
-</div>
- 
 </nav>
-
-
 <div class="banner_home" style="display:none">
   
   <div id="myCarousel" class="carousel slide" data-ride="carousel">
@@ -748,9 +660,6 @@ body {
 </div>
 
 <!---->
-
-
-
             <div class="content" style="display:none">
               <div class="title m-b-md" style="
     font-weight: 400;
@@ -770,115 +679,11 @@ body {
         </div>
 
 <!--Slidder-->
- <!-- Add this css File in head tag-->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" media="all">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet" media="all">
-
-
- <!--  
-        If you want to change #bootstrap-touch-slider id then you have to change Carousel-indicators and Carousel-Control  #bootstrap-touch-slider slide as well
-        Slide effect: slide, fade
-        Text Align: slide_style_center, slide_style_left, slide_style_right
-        Add Text Animation: https://daneden.github.io/animate.css/
-        -->
-
-
-        <div id="bootstrap-touch-slider" class="carousel bs-slider fade  control-round indicators-line" data-ride="carousel" data-pause="hover" data-interval="5000" >
-
-            <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#bootstrap-touch-slider" data-slide-to="0" class="active"></li>
-                <li data-target="#bootstrap-touch-slider" data-slide-to="1"></li>
-                <li data-target="#bootstrap-touch-slider" data-slide-to="2"></li>
-            </ol>
-
-            <!-- Wrapper For Slides -->
-            <div class="carousel-inner" role="listbox">
-
-                <!-- Third Slide -->
-                <div class="item active">
-
-                    <!-- Slide Background -->
-                    <img src="http://img2.zol.com.cn/product/64_940x705/260/ce8lwb67zHqZQ.jpg" alt="Bootstrap Touch Slider"  class="slide-image"/>
-                    <div class="bs-slider-overlay"></div>
-
-                    <div class="container">
-                        <div class="row">
-                            <!-- Slide Text Layer -->
-                            <div class="slide-text slide_style_left">
-                                <h1 data-animation="animated zoomInRight">Khmer Tample Greate</h1>
-                                <p data-animation="animated fadeInLeft">Tample  now touch enable slide.</p>
-                                <a href="http://bootstrapthemes.co/" target="_blank" class="btn btn-default" data-animation="animated fadeInLeft">select one</a>
-                                <a href="http://bootstrapthemes.co/" target="_blank"  class="btn btn-primary" data-animation="animated fadeInRight">select two</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- End of Slide -->
-
-                <!-- Second Slide -->
-                <div class="item">
-
-                    <!-- Slide Background -->
-                    <img src="https://www.ampersandtravel.com/CropUp/940x-/media/466241/Luxury-Indochina-Itinerary-21-.jpg" alt="Bootstrap Touch Slider"  class="slide-image"/>
-                    <div class="bs-slider-overlay"></div>
-                    <!-- Slide Text Layer -->
-                    <div class="slide-text slide_style_center">
-                        <h1 data-animation="animated flipInX">Bootstrap touch slider</h1>
-                        <p data-animation="animated lightSpeedIn">Make Bootstrap Better together.</p>
-                        <a href="http://bootstrapthemes.co/" target="_blank" class="btn btn-default" data-animation="animated fadeInUp">select one</a>
-                        <a href="http://bootstrapthemes.co/" target="_blank"  class="btn btn-primary" data-animation="animated fadeInDown">select two</a>
-                    </div>
-                </div>
-                <!-- End of Slide -->
-
-                <!-- Third Slide -->
-                <div class="item">
-
-                    <!-- Slide Background -->
-                    <img src="https://images.pexels.com/photos/144345/pexels-photo-144345.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" alt="Bootstrap Touch Slider"  class="slide-image"/>
-                    <div class="bs-slider-overlay"></div>
-                    <!-- Slide Text Layer -->
-                    <div class="slide-text slide_style_right">
-                        <h1 data-animation="animated zoomInLeft">Beautiful Animations</h1>
-                        <p data-animation="animated fadeInRight">Lots of css3 Animations to make slide beautiful .</p>
-                        <a href="http://bootstrapthemes.co/" target="_blank" class="btn btn-default" data-animation="animated fadeInLeft">select one</a>
-                        <a href="http://bootstrapthemes.co/" target="_blank" class="btn btn-primary" data-animation="animated fadeInRight">select two</a>
-                    </div>
-                </div>
-                <!-- End of Slide -->
-
-
-            </div><!-- End of Wrapper For Slides -->
-
-            <!-- Left Control -->
-            <a class="left carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="prev">
-                <span class="fa fa-angle-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-
-            <!-- Right Control -->
-            <a class="right carousel-control" href="#bootstrap-touch-slider" role="button" data-slide="next">
-                <span class="fa fa-angle-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-
-        </div> <!-- End  bootstrap-touch-slider Slider -->
-        
-        
-        
-             
-          {{--  <div style="text-align: center;margin-top: 150px; margin-bottom:100px"><h3>Check this in <a href="http://bootstrapthemes.co/demo/resource/BootstrapCarouselTouchSlider/"target="_blank"> Full Screen Preview </a> And Drag the Slider ;)</h3> / Slide Carousel Also <a href="http://bootsnipp.com/snippets/Padax"target="_blank">  Available here </a></div>
-          <div style="text-align: center;margin-top: 150px; margin-bottom:100px"> | <a href="http://bootstrapthemes.co/item/bootstrap-carousel-touch-slider-with-text-animation/"target="_blank"> Snippet By Bootstrapthemes.co </a> |</div>
-            --}}
-
+ 
 <!--Slider-->
-
-
-
-  <div id="goToList" class="content">
+<div id="goToList" class="content">
 <div class="content" style="
-    margin-top: -2px;
+    margin-top: 53px;
 ">
   <h4 style="
     font-size: 24px;
@@ -888,7 +693,51 @@ body {
     background: #fafafb;
     top: 2px;
     margin-top: 0px;
-">List of Tourism Place </h4>
+">
+<div class="container">
+<div class="col-md-12">
+<form class="form-horizontal form-horizontal_x">
+<div class="col-md-12">
+    <div class="form-group">
+     
+      <div class="col-sm-4">
+	 <select name="hotel" id="hotel" required="" class="form-control" onchange="changeFormaction2(this.value);">
+                        <option value="" selected="">Select Hotel</option>
+                                                <option value="67961">Pride Plaza Hotel Ahmedabad</option>
+                                                <option value="67964">The Pride Hotel Bengaluru</option>
+                                                <option value="29191">The Pride Hotel Nagpur</option>
+                                                <option value="67963">The Pride Hotel Chennai</option>
+                                                <option value="29190">The Pride Hotel, Pune</option>
+                                                <option value="67962">Pride Plaza Hotel Kolkata</option>
+                                                <option value="67580">Pride Plaza Hotel Delhi</option>
+                                                <option value="8">Pride Amber Vilas Jaipur</option>
+                                                <option value="65728">Pride Surya Mountain Resort Mcleodganj</option>
+                                                <option value="60533">Pride Sun Village Resort SPA,Goa</option>
+                                                <option value="138227">The Pride Biznotel,Vadodara</option>
+                                        </select>
+	  
+        <!-- <input type="text" class="form-control" id="email" placeholder="Enter email"> -->
+      </div>
+	  
+	  <div class="col-sm-3">          
+        <input type="text" class="form-control" id="pwd" placeholder="Check In">
+      </div>
+	  
+	  <div class="col-sm-3">          
+        <input type="text" class="form-control" id="pwd" placeholder="Check Out">
+      </div>
+	  
+	  <div class="col-sm-2">
+        <button type="submit" class="btn btn-default">Book Now</button>
+      </div>
+    </div>
+	</div>
+	
+  </form>
+  </div>
+</div>
+
+
  </div>
    <div class="container">
     <div clss="col-md-12"> 
